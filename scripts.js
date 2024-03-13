75de5d58a51d65e001692b56912e03ba74fec7f6
2135496 <2135496@LTIN399766>
2024-03-13 16:18:11 +0530

    initial commit

---
+++ scripts.js
@@ -0,0 +1,75 @@
+AOS.init({
+    duration: 1000,
+});
+
+const moreBtn = document.querySelector('.more-btn');
+const expandBtn = document.getElementById('expand-btn');
+const moreBtnIcon = document.getElementById('more-btn-icon');
+
+
+expandBtn.addEventListener('click', () => {
+
+  const isExpanded = moreBtn.classList.contains('expanded-list');
+
+  if (!isExpanded) {
+    moreBtnIcon.classList.add('fa-xmark');
+    moreBtnIcon.classList.remove('fa-bars');
+    moreBtn.classList.add('expanded-list');
+  } else {
+    moreBtn.classList.remove('expanded-list');
+    moreBtnIcon.classList.remove('fa-xmark');
+    moreBtnIcon.classList.add('fa-bars');
+  }
+ 
+});
+
+const nightNode = document.getElementById('night-mode');
+
+nightNode.addEventListener('click', () => {
+   const isLightMode =  document.body.classList.contains('light-mode');
+    if (isLightMode) {
+        document.body.classList.remove('light-mode');   
+        nightNode.innerHTML = ' <i class="fa-solid fa-moon"></i>';
+    } else {  
+        document.body.classList.add('light-mode');  
+        nightNode.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    }
+});
+
+
+const typedText = document.querySelector(".typed-text");
+const cursor = document.querySelector(".cursor");
+const textArray = ['Software Developer',"FullStack Developer", 'Tech Enthusiast', '.Net Developer'];
+charIndex = 0;
+textArrayIndex = 0;
+
+function type() {
+    if (charIndex <= textArray[textArrayIndex].length - 1) {
+      cursor.classList.remove("blink");
+      typedText.textContent += textArray[textArrayIndex].charAt(charIndex);
+      charIndex++;
+      setTimeout(type, 120);
+    } else {
+      cursor.classList.add("blink");
+      setTimeout(erase, 1000);
+    }
+  }
+  
+  function erase() {
+    if (charIndex > 0) {
+      cursor.classList.remove("blink");
+      typedText.textContent = textArray[textArrayIndex].slice(0, charIndex - 1);
+      charIndex--;
+      setTimeout(erase, 80);
+    } else {
+      cursor.classList.add("blink");
+      textArrayIndex++;
+  
+      if (textArrayIndex > textArray.length - 1) {
+        textArrayIndex = 0;
+      }
+      setTimeout(type, 1000);
+    }
+  }
+
+  type();
